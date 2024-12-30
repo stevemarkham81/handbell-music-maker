19cbe53ac29de5b824a34c82536739cacc5aa16d
steve <steve.markham@gmail.com>
2024-12-29 20:57:33 -0500

    code improvements

--- handbell_music_maker.py
+++ handbell_music_maker.py
@@ -1,3 +1,4 @@
+import sys
 from PIL import Image, ImageDraw, ImageFont
 from math import floor
 
@@ -20,6 +21,77 @@ color_map = {'D2': (255, 106, 0),
              }  # modified to use the correct RGB values
 
 songs = {
+    'christmas_bells': {
+        'music': [['B', 'Christ-'], ['B', 'mas'], ['A', 'bells'], ['B', 'are'], ['G', 'ring-'], ['D', 'ing'], ['', ''], ['', ''],
+                  ['G', 'Hear'], ['F#', 'what'], ['G', '-'], ['A', 'they'], ['F#', '-'], ['G', 'say'], ['D', 'to'], ['D', 'you'],
+                  ['G', 'Je-'], ['F#', 'sus'], ['G', ''], ['A', 'is'], ['F#', ''], ['G', 'born'], ['E', ''], ['', ''],
+                  ['C', 'in'], ['D', 'Beth'], ['E', '-'], ['F#', 'le-'], ['G', 'hem'], ['', ''], ['', ''], ['', ''],
+                  ['G', 'In'], ['D', 'Beth'], ['E', '-'], ['F#', 'le-'], ['G', 'hem']
+                 ],
+        'num_lines': 5,
+        'beats_per_line': 8,
+        'num_verses': 1
+    },
+    'beautiful_savior': {
+        'music': [['D', 'Fair', 'Fair', 'Beau-'],[ '', '', '', ''],
+                  ['D', 'is', 'are', 'ti-'],[ 'D', 'the', 'the', 'ful'],[ 'E', 'sun', 'mead', 'Sav-'],
+                  ['C#', '-', '-', '-'],[ 'D', 'shine', 'ows', 'ior'],[ '', '', '', ''],
+                  ['F#', 'Fair', 'Fair', 'Lord'],[ '', '', '', ''],[ 'F#', '-er', '-er', 'of'],[ 'F#', 'the', 'the', 'the'],
+                  ['G', 'moon', 'wood', 'na-'],[ 'E', '-', '-', '-'],[ 'F#', 'light', 'lands', 'tions'],
+                  ['A', 'And', 'Robed', 'Son'],[ '', '', '', ''],[ 'D2', 'all', 'in', 'of'],[ 'B', 'the', 'the', ''],[ 'A', 'stars', 'flow-', 'God'],[ '', '', '', ''],
+                  ['G', '', 'ers', ''],[ 'F#', 'in', 'of', 'and'],[ 'G', 'heav''n', 'bloom', 'Son'],[ '', '', '', ''],[ 'F#', 'a -', 'ing', 'of'],[ '', '', '', ''],
+                  ['E', 'bove', 'spring', 'Man'],[ '', '', '', ''],[ '', '', '', ''],
+                  ['A', 'Je-', 'Je-', 'Thee'],[ '', '', '', ''],[ 'B', 'sus', 'sus', 'will'],[ 'A', 'shines', 'is', 'I'],[ 'A', 'bright', 'fair', 'ho-'],[ 'F#', '-', '-', '-'],[ 'G', 'ter', 'er', 'nor'],[ '', '', '', ''],
+                  ['G', 'Je-', 'Je-', 'Praise'],[ '', '', '', ''],[ 'A', 'sus', 'sus', 'and'],[ 'G', 'shines', 'is', 'give'],[ 'G', 'pur', 'pur', 'glo-'],[ 'E', '-', '-', '-'],[ 'F#', 'er', 'er', 'ry'],
+                  ['F#', 'And', 'He', 'Give'],[ 'F#', 'brings', 'makes', 'praise'],[ 'F#', 'to', 'the', 'and'],[ 'A', 'all', 'sor-', 'glo-'],[ 'G', 'the', 'rowing', 'ry'],
+                  ['F#', 'world', 'spir-', 'Ev-'],[ '', '', '', ''],[ 'E', 'his', 'it', 'er'],[ '', '', '', ''],[ 'D', 'love', 'sing', 'more'],[ '', '', '', ''],
+                 ],
+        'num_lines': 4,
+        'beats_per_line': 15,
+        'num_verses': 3
+    },
+    'happy_birthday': [['C', 'Hap'],[ 'C', 'py'],[ 'D', 'Birth'],[ 'C', 'day'],[ 'F', 'to'],[ 'E', 'you'],[ '', ''],
+                       ['C', 'Hap'],[ 'C', 'py'],[ 'D', 'Birth'],[ 'C', 'day'],[ 'G', 'to'],[ 'F', 'you'],[ '', ''],
+                       ['C', 'Hap'],[ 'C', 'py'],[ 'C2', 'Birth'],[ 'A', 'day'],[ 'F', 'dear'],[ 'E', '___'],[ 'D', '___'],
+                       ['A#', 'Hap'],[ 'A#', 'py'],[ 'A', 'Birth'],[ 'F', 'day'],[ 'G', 'to'],[ 'F', 'you'],[ '', ''],
+                      ],
+    'samuel_tells': [['G', 'Said', 'A-'],[ 'C', 'Sam-', 'cross'],[ 'A#', '-u-', 'the'],[ 'C', '-el', 'sea'],[ '', '', ''],[ 'D', 'With-', 'in'],[ 'C', 'in', 'Beth-'],[ 'A#', '-', ''],[ 'G', 'five', 'le-'],[ 'G', 'years', 'hem'],[ 'G', 'A', 'Lord'],[ 'F', 'night', 'Je-'],[ 'F', 'will', 'sus'],[ 'F', 'be', 'came'],[ 'D#', 'as', 'to'],[ 'G', 'day', 'earth'],[ '', '', ''],
+                     ['G', 'And', 'As'],[ 'C', 'Ba-', 'Sam-'],[ 'A#', '-by', 'u-'],[ 'G', 'Je-', 'el'],[ 'F', '-sus', 'had'],[ 'G', 'will', 'pro-'],[ 'F', '-', '-'],[ 'D#', 'be', 'phe-'],[ 'C', 'born', 'sied'],[ 'C', 'In', 'And'],[ 'D', 'a', ''],[ 'D#', 'land', 'ang-'],[ 'G', 'far', 'els'],[ 'F', 'far', 'sang'],[ 'D#', '', ''],[ 'D', 'a-', 'His'],[ 'C', '-way.', 'birth.'],
+                     ['D', 'Ho-', ''],[ 'D#', 'san-', ''],[ '', '', ''],[ 'D#', 'na!', ''],[ 'F', 'Ho-', ''],[ 'G', 'san-', ''],[ 'A#', '', ''],[ 'A#', 'na!', ''],[ '', '', ''],[ 'A#', 'Oh', ''],[ 'C', 'let', ''],[ 'A#', 'us', ''],[ 'G', 'glad-', ''],[ 'A#', 'ly', ''],[ 'F', 'sing', ''],[ '', '', ''],[ '', '', ''],
+                     ['F', 'How', ''],[ 'D#', 'bless-', ''],[ 'F', 'ed', ''],[ 'G', 'that', ''],[ 'A#', 'out', ''],[ 'C', 'Lord', ''],[ 'A#', 'was', ''],[ 'G', 'born', ''],[ '', '', ''],[ 'A#', 'Let', ''],[ 'G', 'earth', ''],[ 'F', 're-', ''],[ 'D#', 'ceive', ''],[ 'C', 'her', ''],[ 'D#', 'King!', ''],
+                    ],
+    'be_kind': [['C', 'I'],[ 'F', 'want'],[ 'F', 'to'],[ 'F', 'be'],[ 'A', 'kind'],[ 'F', 'to'],[ 'G', 'ev-'],[ 'A#', 'ry-'],[ 'D', 'one,'],
+                ['D', 'For'],[ 'E', 'that'],[ 'G', 'is,'],[ 'A#', 'right,'],[ 'D2', 'you'],[ 'C2', 'see.'],[ '', ''],[ '', ''],[ '', ''],
+                ['C', 'So'],[ 'C', 'I'],[ 'F', 'say'],[ 'F', 'to'],[ 'F', 'my-'],[ 'A', 'self,'],[ 'F', '"Re-'],[ 'G', 'mem'],[ 'A#', 'ber'],[ 'D', 'this:'],
+                ['E', 'Kind-'],[ 'E', 'ness'],[ 'E', 'be-'],[ 'A#', 'gins'],[ 'E', 'with'],[ 'F', 'me."'],
+               ],
+    'we_are_different': [['E', 'I'],[ 'C', 'know'],[ 'G', 'you'],[ 'B', 'and'],[ 'B', 'you'],[ 'C', 'know'],[ 'A', 'me.'],[ '', ''],[ '', ''],[ '', ''],[ '', ''],
+                         ['B', 'We'],[ 'B', 'are'],[ 'C', 'as'],[ 'D', 'dif-'],[ 'C', 'ferent,'],[ 'B', 'as'],[ 'A', 'the'],[ 'A', 'sun'],[ 'B', 'and'],[ 'A', 'the'],[ 'G', 'sea.'],
+                         ['E', 'I'],[ 'C', 'know'],[ 'G', 'you'],[ 'B', 'and'],[ 'B', 'you'],[ 'C', 'know'],[ 'A', 'me.'],[ '', ''],[ '', ''],[ '', ''],[ '', ''],
+                         ['B', 'And'],[ 'B', 'that''s,'],[ 'C', 'the'],[ 'D', 'way'],[ 'C', 'that'],[ 'B', 'it''s'],[ 'D', 'sup-'],[ 'D', 'posed'],[ 'C', 'to'],[ 'C', 'be.'],
+                        ],
+    'love_one_another': [['F', 'As'],[ 'G', 'I'],[ 'A', 'have'],[ 'C', 'loved'],[ 'C', 'you,'],[ 'D', 'Love'],[ 'E', 'one'],[ 'F', 'an-'],[ 'D', 'oth'],[ 'D', '-er.'],
+                         ['G', 'This'],[ 'A', 'new'],[ 'A#', 'com-'],[ 'G', 'man'],[ 'G', '-ment:'],[ 'F', 'Love'],[ 'E', 'one'],[ 'D', 'an-'],[ 'C', 'oth'],[ 'C', '-er.'],
+                         ['A', 'By'],[ 'A#', 'this'],[ 'C', 'shall'],[ 'A', 'men'],[ 'D', 'know'],[ 'G', 'Ye'],[ 'G', 'are'],[ 'A', 'my'],[ 'A#', 'dis-'],[ 'G', 'ci-'],[ 'G', 'ples,'],
+                         ['F', 'If'],[ 'G', 'ye'],[ 'A', 'have'],[ 'F', 'love'],[ 'D', 'One'],[ 'E', 'to'],[ 'F', 'an-'],[ 'G', 'oth'],[ 'F', '-er.'],
+                        ],
+    'be_like_jesus_verse': [['C','I''m'],['D','try-'],['D#','ing'],['F','to'],['F','be'],['C2','like'],['A#','Je-'],['','-'],['D#','sus'],['',''],['',''],
+                            ['D#','I''m'],['D','fol-'],['D#','low-'],['F','ing'],['G','in'],['D#','his'],['C','ways.'],['',''],['',''],['',''],['',''],
+                            ['C','I''m'],['D','try-'],['D#','ing'],['F','to'],['F','love'],['A#','as'],['A#','he'],['','-'],['D#','did'],['',''],['',''],
+                            ['D#','in'],['D','all'],['D#','that'],['F','I'],['G','do'],['G','and'],['A','say.'],['',''],['',''],['',''],['',''],
+                            ['A','At'],['A#','times'],['A#','I am'],['A','tempt-'],['F','ed'],['D','to'],['G','make'],['F','a'],['D#','wrong'],['F','choice'],['',''],
+                            ['F','But I'],['G','try'],['G','to'],['A','list-'],['F','en'],['F','as the'],['G','still'],['A#','small'],['C2','voice'],['A#','whisp-'],['C2','ers:'],['',''],['',''],['',''],['',''],
+                           ],
+    'be_like_jesus_chorus': [['D2','Love'],['D2','one an-'],['D2','other-'],['C2','er'],['A#','as'],['A#','Je-'],['D#','sus'],['G','loves'],['F','you.'],['',''],
+                             ['G','Try'],['A','to'],['A#','show'],['F','kind-'],['A#','ness'],['A#','in'],['A#','all'],['C2','that'],['D2','you'],['C2','do.'],
+                             ['C2','Be'],['D2','gen-'],['D2','tle and'],['D2','lov-'],['C2','ing'],['A#','in'],['A#','deed'],['A','and'],['G','in'],['F','thought,'],
+                             ['D#','For'],['D','these'],['A#','are'],['D','the'],['D#','things'],['C2','Je-'],['A','sus'],['A#','taught.'],
+                            ],
+    'be_kind': [['C','I'],['F','want'],['F','to'],['F','be'],['A','kind'],['F','to'],['G','ev-'],['A#','ry-'],['D','one,'],
+                ['D','For'],['E','that'],['G','is,'],['A#','right,'],['D2','you'],['C2','see.'],['',''],['',''],['',''],
+                ['C','So'],['C','I'],['F','say'],['F','to'],['F','my-'],['A','self,'],['F','"Re-'],['G','mem'],['A#','ber'],['D','this:'],
+                ['E','Kind-'],['E','ness'],['E','be-'],['A#','gins'],['E','with'],['F','me."'],
+               ],
     'silent_night': [['F', 'Si-'], ['G', '-'], ['F', 'lent'], ['D', 'night'], ['', ''], ['F', 'Ho-'], ['G', '-'], ['F', 'ly'], ['D', 'night'], ['', ''],
                      ['C', 'All'], ['', ''], ['C', 'is'], ['A', 'calm,'], ['', ''], ['A#', 'All'], ['', ''], ['A#', 'is'], ['F', 'bright'], ['', ''],
                      ['G', 'Round'], ['G', 'yon'], ['A#', 'vir-,'], ['A', '-'], ['G', 'gin'], ['F', 'mo-'], ['G', 'ther'], ['F', 'and'], ['D', 'Child'], ['', ''],
@@ -27,165 +99,101 @@ songs = {
                      ['C', 'Sleep'], ['C', 'in'], ['D#', 'hea-,'], ['', ''], ['C', 'ven-'], ['A', 'ly'], ['A#', 'pea-'], ['', ''], ['D', 'ce'], ['', ''],
                      ['A#', 'Sle-'], ['F', 'ep'], ['D', 'in'], ['F', 'hea-,'], ['D#', 'ven-'], ['C', 'ly'], ['A#', 'peace']
                     ],
-
     'away_in_a_manger': [
         ['C', 'A-'], ['F', 'way'], ['F', 'in'], ['G', 'a-'], ['A', '-'],
         ['F', 'man-'], ['F', 'ger'], ['A', 'No-'], ['A#', '-'], ['C', 'crib'],
         ['C', 'for'], ['D', 'a'], ['A#', 'bed'],
-
         ['G', 'The-'], ['A', '-'], ['A#', 'lit-'], ['A#', 'tle'], ['C', 'Lord'],
         ['A', 'Je-'], ['A', 'sus'], ['F', 'lay-'], ['A', '-'], ['G', 'down'],
         ['D', 'his'], ['F', 'sweet'], ['E', 'head'],
-
         ['C', 'The'], ['F', 'stars'], ['F', 'in'], ['G', 'the-'], ['A', '-'],
         ['F', 'hea-'], ['F', 'vens'], ['A', 'Looked-'], ['A#', '-'], ['C', 'down'],
         ['C', 'where'], ['D', 'he'], ['A#', 'lay'],
-
         ['G', 'The-'], ['A', '-'], ['A#', 'lit-'], ['A#', 'tle'], ['C', 'Lord'],
         ['A', 'Je-'], ['A', 'sus'], ['F', 'a-'], ['A', '-'], ['G', 'sleep'],
         ['D', 'on'], ['E', 'the'], ['F', 'hay']
     ],
-
     'hark_the_herald': [
         ['C', 'Hark'], ['F', 'the'], ['F', 'her-'], ['E', 'ald'], ['F', 'an-'], ['A', 'gels'], ['A', 'sing-'], ['G', '-'],
-
         ['C', 'glo,'], ['C', 'ry'], ['C', 'to'], ['A#', 'the'], ['A', 'new-'], ['G', 'born'], ['A', 'king'], ['', ''],
-
         ['C', 'Peace'], ['F', 'on'], ['F', 'earth'], ['E', 'and'], ['F', 'mer-'], ['A', 'cy'], ['A', 'mi-'], ['G', 'ld'],
-
         ['C', 'God'], ['G', 'and'], ['G', 'sin-'], ['E', 'ners'], ['E', 're,'], ['D', 'con-'], ['C', 'ciled'], ['', ''],
-
         ['C', 'Joy-'], ['C', 'ful'], ['C', 'all'], ['F', 'ye'], ['A#', 'na-'],
         ['A', 'tions'], ['A', 'ri-'], ['G', 'se'],
-
         ['C', 'Join'], ['C', 'the'], ['C', 'tri-'], ['F', 'umph'], ['A#', 'of'],
         ['A', 'the'], ['A', 'ski-'], ['G', 'es'],
-
         ['D', 'With'], ['D', 'th an-'], ['D', 'gel-'], ['C', 'ic'], ['A#', 'hosts'],
         ['A', 'pro-'], ['A#', 'claim'],
-
         ['G', 'Christ'], ['A', 'is-'], ['A#', '-'], ['C', 'born'], ['F', 'in'],
         ['F', 'Beth-'], ['G', 'le-'], ['A', 'hem'],
-
         ['D', 'Hark'], ['D', 'the'], ['D', 'her-'], ['C', 'ald'], ['A#', 'an-'],
         ['A', 'gels'], ['A#', 'sing'],
-
         ['G', 'Christ'], ['A', 'is-'], ['A#', '-'], ['C', 'born'], ['F', 'in'],
         ['F', 'Beth-'], ['G', 'le-'], ['F', 'hem']
     ]
 }
 
-def draw_note(draw: ImageDraw, x: int, y: int, color: tuple[int, int, int], ring: bool) -> tuple[int, int]:
-    if ring:
-        print(f'Black circle for {color}')
+def draw_note(draw: ImageDraw, x: int, y: int, note: str, radius, font) -> tuple[int, int]:
+    color = color_map[note]
+    ring_thinkness = max(radius//5, 5)
+    if '#' in note:
         draw.ellipse((x-radius, y-radius, x+radius, y+radius), fill=color_map['black'])
         draw.ellipse((x-radius+ring_thinkness, y-radius+ring_thinkness, x+radius-ring_thinkness, y+radius-ring_thinkness), fill=color)
     else:    
         draw.ellipse((x-radius, y-radius, x+radius, y+radius), fill=color)
+    w,h = font.getbbox(note)[2:4]
+    draw.text((x-w/2, y-h/2), note, fill='white', font=font)
 
-img_resolution = (1920, 1080)
-image = Image.new('RGB', img_resolution, color=color_map['white'])
-draw = ImageDraw.Draw(image)
+def make_music(song):
+    img_resolution = (1920, 1080)
+    image = Image.new('RGB', img_resolution, color=color_map['white'])
+    draw = ImageDraw.Draw(image)
 
-beats_per_line = 10
-num_lines = 10
-num_verses = 1
-
-radius = floor(min(img_resolution[0]/(beats_per_line*3+1), img_resolution[1]/(2+num_lines*(3+num_verses))))
-
-x_step = floor(img_resolution[0]/beats_per_line)
-y_step = floor(img_resolution[1]/num_lines)
-
-
-ring_thinkness = max(radius//5, 5)
-
-print(f'radius: {radius}')
-
-x = radius*2
-y = radius*2
-
-font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf', size=radius)
-
-for beat in songs['silent_night']:
-    note, *lyrics = beat
-
-    if note in color_map:
-        draw_note(draw, x, y, color_map[note], '#' in note)
-    
-    if lyrics:
-        for verse,words in enumerate(lyrics):
-            w = font.getbbox(words)[2]
-            draw.text((x-w/2, y+(radius*(verse+2))), words, fill='black', font=font)
-    
-    x += x_step
-    
-    if x > img_resolution[0]:
-        x = radius*2
-        y += y_step
-
-print(font.size)
-image.save('pil_test.png')
-
-exit()
-
-
-x_max = 12
-
-fig, ax = plt.subplots(figsize=(15, 220))
-titleh = plt.title('Silent Night', fontsize=20, va='bottom')
-ax.set_xlim(-1, x_max+1)
-ax.set_ylim(0, 12)
-ax.set_xticks([])
-ax.set_yticks([])
-
-start_position = 0
-if len(songs['silent_night'][0]) == 2:
-    delta_y = 1.8
-elif len(songs['silent_night'][0]) == 3:
-    delta_y = 2.3
-elif len(songs['silent_night'][0]) == 4:
-    delta_y = 2.35
-else:
-    print("Too many verses?")
-
-y = 11.2 - delta_y
-note_dy = 0.2 + 0.3 * (len(songs['silent_night'][0]) - 1)
-
-for beat in songs['silent_night']:
-    note = beat[0]
-    if not note:
-        start_position += 1.25
-        if start_position > x_max-1:
-            start_position = 0
-            y -= delta_y
-        continue
-
-    if len(note) == 2:  # accidentals get black rings around the note
-        rh = plt.Rectangle((start_position, y+note_dy), 1, 1,
-                           color=note_color_map[note][0]/255)
-        txt_offset = 0.25
+    if 'music' in songs[song]:
+        music = songs[song]['music']
+        num_lines = songs[song]['num_lines']
+        num_verses = songs[song]['num_verses']
+        beats_per_line = songs[song]['beats_per_line']
     else:
-        rh = plt.Rectangle((start_position, y+note_dy), 1, 1,
-                           color=note_color_map[note][0]/255)
-        txt_offset = 0.32
+        music = songs[song]
+        beats_per_line = 10
+        num_lines = 10
+        num_verses = 1
 
-    th = ax.text(start_position, y+note_dy/2,
-                 songs['silent_night'][1][i+1], ha='center', va='bottom')
+    radius = floor(min(img_resolution[0]/(beats_per_line*3+1), img_resolution[1]/(2+num_lines*(3+num_verses))))
 
-    e = th.get_window_extent()
-    over = (e.xmin + e.width/2) - (start_position + 0.5)
-    if over >= 0:
-        plt.setp(rh, xy=(start_position+over, y+note_dy))
-    else:
-        plt.setp(th, xy=(start_position-over, 0))
+    x_start = radius*2
+    y_start = radius*2
+    x_step = radius*3
+    y_step = radius*(3+num_verses)
 
-    rpos = plt.gca().get_window_extent()[1]
-    ax.text(rpos[1]+txt_offset, (rpos[2]+note_dy) /
-            2, note, color='white', ha='center')
+    x = x_start
+    y = y_start
 
-    start_position += 1 + e.width/2
-    if start_position > x_max-1:
-        start_position = 0
-        y -= delta_y
+    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf', size=radius)
 
-plt.show()
+    for beat in music:
+        note, *lyrics = beat
+
+        if note in color_map:
+            draw_note(draw, x, y, note, radius, font)
+        
+        if lyrics:
+            for verse,words in enumerate(lyrics):
+                w,h = font.getbbox(words)[2:4]
+                draw.text((x-w/2, y+(radius*(verse+2))-h/2), words, fill='black', font=font)
+        
+        x += x_step
+        
+        if x >= x_start + (beats_per_line*x_step):
+            x = x_start
+            y += y_step
+
+    image.save(f'{song}.png')
+
+if __name__ == "__main__":
+    song = 'christmas_bells'
+    if len(sys.argv) > 1:
+        song = sys.argv[1]
+    
+    make_music(song)
